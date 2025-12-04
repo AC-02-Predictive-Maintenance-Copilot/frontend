af1c5c966fa4dc7a40df16b8ddfc890c4df9bf43
ZulhanF <zulhanfadil@gmail.com>
2025-12-04 10:24:56 +0700

    Perbarui BASE_URL ke relative path dan tambahkan konfigurasi proxy untuk API di Vite.

--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://backend-ruddy-eta.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path,
+      },
+    },
+  },
 })
